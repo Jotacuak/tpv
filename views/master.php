da71b55e94284863af57cc9a449055df73e5e4cf
Jotacuak <jaimejsastre@gmail.com>
2022-06-06 18:52:38 +0200

    start html

---
+++ views/master.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+	<head> 
+		<meta charset="utf-8"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<meta name="description" content="">
+		<meta name="keywords" content=""> 
+	</head>
+
+    <header>
+        <div class="header-fixed">
+            <div class="header-elements">
+
+                <div class="header-element-button">
+                    <button class="back-button">
+                        <svg viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12M18,11H10L13.5,7.5L12.08,6.08L6.16,12L12.08,17.92L13.5,16.5L10,13H18V11Z" />
+                        </svg>
+                    </button>
+                </div>
+
+                <div class="header-element-button">
+                    <button class="home-button">
+                        <svg viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M19.07,4.93C17.22,3 14.66,1.96 12,2C9.34,1.96 6.79,3 4.94,4.93C3,6.78 1.96,9.34 2,12C1.96,14.66 3,17.21 4.93,19.06C6.78,21 9.34,22.04 12,22C14.66,22.04 17.21,21 19.06,19.07C21,17.22 22.04,14.66 22,12C22.04,9.34 21,6.78 19.07,4.93M17,12V18H13.5V13H10.5V18H7V12H5L12,5L19.5,12H17Z" />
+                        </svg>
+                    </button>
+                </div>
+
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="two-columns-aside">
+            <div class="column-aside">
+
+                <div class="function-elements">
+
+                    <div class="function-element-button">
+                        <button class="clear-button">
+                            <svg viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M14.04,12H10V11H5.5A3.5,3.5 0 0,1 2,7.5A3.5,3.5 0 0,1 5.5,4C6.53,4 7.45,4.44 8.09,5.15C8.5,3.35 10.08,2 12,2C13.92,2 15.5,3.35 15.91,5.15C16.55,4.44 17.47,4 18.5,4A3.5,3.5 0 0,1 22,7.5A3.5,3.5 0 0,1 18.5,11H14.04V12M10,16.9V15.76H5V13.76H19V15.76H14.04V16.92L20,19.08C20.58,19.29 21,19.84 21,20.5A1.5,1.5 0 0,1 19.5,22H4.5A1.5,1.5 0 0,1 3,20.5C3,19.84 3.42,19.29 4,19.08L10,16.9Z" />
+                            </svg>
+                        </button>
+                    </div>
+
+                    <div class="function-element-ticket"></div>
+
+                    <div class="function-element-button">
+                        <button class="pay-button">Sacar ticket</button>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="column-main">
+                <div class="nav-elements"></div>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+    </footer>
+
+</html>
